77b1689e5626029dee59b297697d46eb3b07e780
calorsaleixo <carloseduardo1400.800@gmail.com>
2024-06-18 22:17:01 -0300

    primeiro projeto

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { useRouter } from 'vue-router';
+defineProps({
+    titulo: {
+        type: String,
+        required: true
+    },
+    logo: {
+        type: String,
+        required: true
+    },
+})
+</script>
+
+<template>
+    <nav>
+        <div id="cab">
+            <div id="div_logo">
+                <span>
+                    <img :src="logo" >
+                </span>
+            </div>
+            <div id="login" @submit.prevent="goToPosts">
+                <button id="b" class="btn btn-primary">Sair</button>
+            </div>
+            <div id="div_titulo">
+                <span>{{ titulo }}</span>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        background-color: white;
+        color: white;
+        padding: 20px;
+        right: 0px;
+        width: 90%;
+        position: absolute;
+        top: 0px;
+    }
+    #cab{
+        display: flex;
+        justify-content: space-between;
+    }
+    #div_logo{
+        display: flex;
+        align-items: center;
+        margin: auto;
+    }
+    #div_titulo{
+        color:black;
+    }
+    #login{
+        text-align: right;
+        width: 100%;
+    }
+    #b{
+        width: 75px
+    }
+</style>
---
+++ src/components/CorpoBlog2.vue
@@ -0,0 +1,55 @@
+<script setup>
+defineProps({
+  titulo: {
+    type: String,
+    required: true
+  },
+  txt: {
+    type: String,
+    required: true
+  },
+  post: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="largura">
+        <div class="cab">
+            <div class="div_logo">
+                <span>
+                    <img :src="post">
+                </span>
+            </div>
+            <div id="div_titulo">
+                <span>{{ titulo }}</span>
+            </div>
+            <div id="div_txt">
+                <span>{{ txt }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#div_titulo{
+    color: red;
+    font-family: 'Times New Roman';
+}
+#div_txt{
+    font-family: 'Arial';
+}
+.cab{
+    text-align: justify;
+}
+.largura{
+    max-width: 90%;
+    align-items: center;
+    margin: auto;
+}
+.div_logo{
+    max-width: 90%;
+}
+</style>
